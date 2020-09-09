80dc9392d07b450dbcd5ae5f08dc9b73a704b203
ybaranov <baranovy@ukr.net>
2020-09-09 15:53:38 +0300

    commented test.

--- name_counter/src/test/java/com/big_id/name_counter/BasicTest.java
+++ name_counter/src/test/java/com/big_id/name_counter/BasicTest.java
@@ -4,5 +4,10 @@ import org.junit.jupiter.api.Test;
 
 public class BasicTest {
 
+	//@Test
+	//public void test_0() {
+	//	NameCounterApplicationSimple.main(new String[] {"http://norvig.com/big.txt", 
+	//			"James,John,Robert,Michael,William,David,Richard,Charles,Joseph,Thomas,Christopher,Daniel,Paul,Mark,Donald,George,Kenneth,Steven,Edward,Brian,Ronald,Anthony,Kevin,Jason,Matthew,Gary,Timothy,Jose,Larry,Jeffrey,Frank,Scott,Eric,Stephen,Andrew,Raymond,Gregory,Joshua,Jerry,Dennis,Walter,Patrick,Peter,Harold,Douglas,Henry,Carl,Arthur,Ryan,Roger"});
+	//}
 	
 }
